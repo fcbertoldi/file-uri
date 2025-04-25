443374a3c64e1fecc0d17a685fe8702d9c3d7cf7
Fernando Costa Bertoldi <fcbertoldi@gmail.com>
2025-04-25 17:12:46 -0300

    Update cram.sh

--- tests/cram.sh
+++ tests/cram.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 
 # Usage:
-#   cram [OPTIONS] <test-file>
+#   cram [OPTIONS] <test-file>...
 #
 # Options:
 #   -h --help      Show this help message and exit
@@ -12,6 +12,7 @@
 usage() {
 	script_name=$(basename "$0")
 	echo "Usage:"
+	echo "  $script_name <test-file>..."
 	echo "  $script_name -h | --help"
 	echo ""
 	echo "Options:"
@@ -22,15 +23,87 @@ usage() {
 }
 
 compare_outputs() {
-	if ! diff -q "$expected_output_file" "$output_file" > /dev/null; then
-		printf "Error: Output differs from expected\n" >&2
-		diff -u "$expected_output_file" "$output_file" >&2
+	if ! diff -q "$expected_output_file" "$output_file" >/dev/null; then
+		printf "%s\n\nError: Output differs from expected\n\n%s: \$ %s\n" "$test_file" "$cmd_line" "$cmd" >&2
+		diff -u "$expected_output_file" "$output_file" | tail -n +3 >&2
 		exit 1
 	fi
 }
 
 exec_cmd() {
-	eval "$cmd" > "$output_file"
+	eval "$cmd" >"$output_file" 2>&1
+}
+
+process_test_file() {
+	: >"$output_file"
+	: >"$expected_output_file"
+
+	test_file="$1"
+	cmd_build=''
+	output_build=''
+	cur_line=1
+
+	while IFS= read -r line; do
+
+		if echo "$line" | grep -E '^  \$' >/dev/null; then
+
+			if [ -n "$cmd_build" ]; then
+				exec_cmd
+			fi
+
+			if [ -n "$output_build" ]; then
+				compare_outputs
+			fi
+			cmd_build=1
+			output_build=''
+			cmd_line="$cur_line"
+			cmd="${line#  $ }"
+
+		elif echo "$line" | grep -E '^  >' >/dev/null; then
+
+			cmd="$cmd ${line#  > }"
+
+		elif echo "$line" | grep -E '^  ' >/dev/null; then
+
+			if [ -n "$cmd_build" ]; then
+				exec_cmd
+			fi
+
+			cmd_build=''
+			if [ -z "$output_build" ]; then
+				echo "${line#  }" >"$expected_output_file"
+				output_build=1
+			else
+				echo "${line#  }" >>"$expected_output_file"
+			fi
+
+		else
+
+			if [ -n "$cmd_build" ]; then
+				exec_cmd
+			fi
+
+			if [ -n "$output_build" ]; then
+				compare_outputs
+			fi
+
+			cmd_build=''
+			output_build=''
+
+		fi
+
+		cur_line=$((cur_line + 1))
+
+	done <"$test_file"
+
+	if [ -n "$cmd_build" ]; then
+		exec_cmd
+	fi
+
+	if [ -n "$expected_output" ]; then
+		compare_outputs
+	fi
+
 }
 
 while getopts ":h-" opt; do
@@ -60,8 +133,8 @@ done
 
 shift "$((OPTIND - 1))"
 
-if [ -z "$1" ]; then
-	echo "Test file not provided" >&2
+if [ $# -eq 0 ]; then
+	echo "Error: No test files provided" >&2
 	usage
 	exit 1
 fi
@@ -70,63 +143,11 @@ output_file=$(mktemp)
 expected_output_file=$(mktemp)
 trap 'rm -f "$output_file" "$expected_output_file"' EXIT INT TERM
 
-cmd_build=''
-output_build=''
-
-while IFS= read -r line; do
-
-	if echo "$line" | grep -E '^  \$' >/dev/null; then
-
-		if [ -n "$cmd_build" ]; then
-			exec_cmd
-		fi
-
-		if [ -n "$output_build" ]; then
-			compare_outputs
-		fi
-		cmd_build=1
-		output_build=''
-		cmd="${line#  $ }"
-
-	elif echo "$line" | grep -E '^  >' >/dev/null; then
-
-		cmd="$cmd ${line#  > }"
-
-	elif echo "$line" | grep -E '^  ' >/dev/null; then
-
-		if [ -n "$cmd_build" ]; then
-			exec_cmd
-		fi
-
-		cmd_build=''
-		if [ -z "$output_build" ]; then
-			echo "${line#  }" >"$expected_output_file"
-			output_build=1
-		else
-			echo "${line#  }" >>"$expected_output_file"
-		fi
-
-	else
-
-		if [ -n "$cmd_build" ]; then
-			exec_cmd
-		fi
-
-		if [ -n "$output_build" ]; then
-			compare_outputs
-		fi
-
-		cmd_build=''
-		output_build=''
-
+for test_file in "$@"; do
+	if [ ! -f "$test_file" ]; then
+		echo "Error: Test file not found: $test_file" >&2
+		exit 1
 	fi
 
-done <"$1"
-
-if [ -n "$cmd_build" ]; then
-	exec_cmd
-fi
-
-if [ -n "$expected_output" ]; then
-	compare_outputs
-fi
+	process_test_file "$test_file"
+done
